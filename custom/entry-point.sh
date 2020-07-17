8ecaea640ce7d93e54ff634af88ad0d5d6c2ea65
nonom <nonomartinez@gmail.com>
2020-07-18 00:23:50 +0200

    Experimental

--- custom/entry-point.sh
+++ custom/entry-point.sh
@@ -5,7 +5,11 @@
 # L2J Server is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/. 
 
-echo "Welcome to L2J Server"
+# This file will override the install entry-point after running 
+# `docker cp custom/. l2j-server-docker:/.`
+# Check the README.md file for more information
+
+echo "Welcome Again to L2J Server (2004-2020)"
 
 cd /opt/l2j/server/login/
 sh startLoginServer.sh
